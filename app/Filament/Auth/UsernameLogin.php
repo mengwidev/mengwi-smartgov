5d2dd64c1d105c4d497fade00920c717fa0e8a60
bagusmahendra85 <bagus.putramahendra@gmail.com>
2025-01-11 01:53:43 +0800

    FEAT: add username login option in filament dashboard

---
+++ app/Filament/Auth/UsernameLogin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Auth;
+
+use Filament\Pages\Auth\Login;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Component;
+use Illuminate\Validation\ValidationException;
+
+class UsernameLogin extends Login
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getLoginFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getRememberFormComponent(),
+                    ])
+                    ->statePath('data')
+            ),
+        ];
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label(__('Username / Email'))
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL)
+            ? 'email'
+            : 'username';
+        return [
+            $login_type => $data['login'],
+            'password' => $data['password'],
+        ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.login' => __(
+                'filament-panels::pages/auth/login.messages.failed'
+            ),
+        ]);
+    }
+}
