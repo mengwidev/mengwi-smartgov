81db5f1b1b3184393af982a0dd4d84641d8b54c7
bagusmahendra85 <bagus.putramahendra@gmail.com>
2025-01-29 21:44:14 +0800

    FEAT: authorization feature and fix translation for ID

---
+++ lang/vendor/filament-shield/id/filament-shield.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Table Columns
+    |--------------------------------------------------------------------------
+    */
+
+    'column.name' => 'Nama',
+    'column.guard_name' => 'Nama Guard',
+    'column.roles' => 'Roles',
+    'column.permissions' => 'Permission',
+    'column.updated_at' => 'Terakhir Diubah',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Fields
+    |--------------------------------------------------------------------------
+    */
+
+    'field.name' => 'Nama',
+    'field.guard_name' => 'Nama Guard',
+    'field.permissions' => 'Permission',
+    'field.select_all.name' => 'Pilih Semua',
+    'field.select_all.message' => 'Aktifkan semua izin yang <span class="font-medium text-primary">Tersedia</span> untuk Role ini.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Pengaturan',
+    'nav.role.label' => 'Role & Permission',
+    'nav.role.icon' => 'heroicon-o-shield-check',
+    'resource.label.role' => 'Role',
+    'resource.label.roles' => 'Roles',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Section & Tabs
+    |--------------------------------------------------------------------------
+    */
+
+    'section' => 'Section',
+    'resources' => 'Resource',
+    'widgets' => 'Widget',
+    'pages' => 'Page',
+    'custom' => 'Custom',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages
+    |--------------------------------------------------------------------------
+    */
+
+    'forbidden' => 'Anda tidak punya izin akses',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Resource Permissions' Labels
+    |--------------------------------------------------------------------------
+    */
+
+    // 'resource_permission_prefixes_labels' => [
+    //     'view' => 'View',
+    //     'view_any' => 'View Any',
+    //     'create' => 'Create',
+    //     'update' => 'Update',
+    //     'delete' => 'Delete',
+    //     'delete_any' => 'Delete Any',
+    //     'force_delete' => 'Force Delete',
+    //     'force_delete_any' => 'Force Delete Any',
+    //     'restore' => 'Restore',
+    //     'replicate' => 'Replicate',
+    //     'reorder' => 'Reorder',
+    //     'restore_any' => 'Restore Any',
+    // ],
+];
