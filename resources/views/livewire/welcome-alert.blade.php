c0598d518cff79cf3336c24bc14094cb251dc172
bagusmahendra85 <bagus.putramahendra@gmail.com>
2025-03-15 22:42:10 +0800

    chore: updating dependencies

--- resources/views/livewire/welcome-alert.blade.php
+++ resources/views/livewire/welcome-alert.blade.php
@@ -12,7 +12,7 @@
         </div>
     </div> --}}
     <!-- message wrapper -->
-    <div class="p-10 space-y-8 bg-white opacity-80">
+    <div class="p-10 space-y-8 bg-white text-gray-700">
         <div>
             <!-- brand -->
             <div class="flex items-center space-x-2">
@@ -38,7 +38,7 @@
         <div class="text-center">
             <a href="{{ url('/admin/login') }}">
                 <button type="button"
-                    class="px-8 py-2 text-sm font-medium text-center text-white rounded-lg bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">Login
+                    class="px-8 py-2 text-sm font-medium text-center text-white rounded-lg bg-gradient-to-bl from-emerald-400 via-emerald-500 to-emerald-600 hover:ring-4 hover:ring-emerald-200">Login
                     Admin</button>
             </a>
         </div>
