a6435bd1715bfaf38b9c4165fd695af0a9511c45
bagusmahendra85 <bagus.putramahendra@gmail.com>
2025-03-15 22:03:19 +0800

    chore: fixing welcome page styles

--- resources/views/livewire/welcome-alert.blade.php
+++ resources/views/livewire/welcome-alert.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-2xl overflow-hidden rounded-lg shadow-lg border border-white">
+<div class="max-w-3xl overflow-hidden rounded-lg shadow-lg border border-white">
     <!-- top bar -->
     {{-- <div class="py-2 bg-slate-800">
         <!-- button wrapper -->
@@ -17,11 +17,7 @@
             <!-- brand -->
             <div class="flex items-center space-x-2">
                 <!-- logo -->
-                <img
-                    src="{{ asset('assets/desa-mengwi.png') }}"
-                    alt="logo desa mengwi"
-                    class="w-12"
-                />
+                <img src="{{ asset('assets/desa-mengwi.png') }}" alt="logo desa mengwi" class="w-12" />
                 <!-- brand name -->
                 <div>
                     <h1 class="text-xl font-semibold">Mengwi SmartGov App</h1>
@@ -41,7 +37,9 @@
         <!-- cta button -->
         <div class="text-center">
             <a href="{{ url('/admin/login') }}">
-                <button type="button" class="px-8 py-2 text-sm font-medium text-center text-white rounded-lg bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">Login Admin</button>
+                <button type="button"
+                    class="px-8 py-2 text-sm font-medium text-center text-white rounded-lg bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">Login
+                    Admin</button>
             </a>
         </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel(['resources/css/app.css', 'resources/js/app.js']),
-
+        laravel(['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css']),
     ],
 });
